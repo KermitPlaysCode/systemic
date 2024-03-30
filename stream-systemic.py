25e08a823fc2abd79b9cc019a96f61f4280f553d
Olivier <78881889+KermitPlaysCode@users.noreply.github.com>
2024-03-30 15:55:13 +0100

    Export nodes

--- stream-systemic.py
+++ stream-systemic.py
@@ -13,7 +13,7 @@ ConfigIo = {
         '-': '#3d753e'
         },
     'Container': None,
-    'df': None,
+    'df_edges': None,
 }
 
 ConfigDigraph = {
@@ -34,10 +34,10 @@ ConfigDigraph = {
 # Interface language
 global ConfigLang
 ConfigLang = {
-    'Label_update': "Mettre à jour",
+    'LabelGetNodes': "Liste des Noeuds (csv)",
     'Title_data': 'Données',
     'Title_graph': 'Graphique',
-    'Label_download': 'Télécharger'
+    'LabelGetEdges': 'Télécharger les Edges (csv)'
 }
 
 # Initialize graph and data
@@ -48,23 +48,29 @@ ConfigDigraph['graph'] = graphviz.Digraph(
     engine=ConfigDigraph['RenderEngine'],
     graph_attr=ConfigDigraph['RenderOptions'][ConfigDigraph['RenderEngine']]
     )
-df_e = pd.read_csv(ConfigIo['Edges_csv_init'], delimiter=',', header=0)
+DfEdges = pd.read_csv(
+    ConfigIo['Edges_csv_init'],
+    delimiter=',',
+    header=0,
+    dtype={'node1': str, 'node2': str, "effet": str}
+    )
 
 # Function to save edges and refresh graph
 def UpdateEdges() -> None:
     """Write down df to csv
     Update the graph based on data"""
     # Write df to CSV
-    ConfigIo['df'].to_csv(ConfigIo['Edges_csv'])
+    ConfigIo['df_edges'].to_csv(ConfigIo['Edges_csv'], index=False)
     # Re-create diagram
     ConfigDigraph['graph'].clear()
-    for index, row in ConfigIo['df'].iterrows():
+    for index, row in ConfigIo['df_edges'].iterrows():
         n1 = row['Node1']
         n2 = row['Node2']
         effect = row['Effet']
         if n1 not in ["", None] and n2 not in ["", None] and effect != None and effect.strip() in ['+','-']:
             effect = effect.strip()
-            ConfigDigraph['graph'].edge(n1,n2,
+            ConfigDigraph['graph'].edge(
+                n1,n2,
                 label=effect,
                 color=ConfigIo['Effet'][effect]
                 )
@@ -75,6 +81,7 @@ def UpdateEdges() -> None:
 
 @st.cache_data
 def df_to_csv(df):
+    """Transform dataframe into CSV ready for download"""
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     # https://docs.streamlit.io/library/api-reference/widgets/st.download_button
     return df.to_csv().encode('utf-8')
@@ -84,22 +91,31 @@ col_data, col_graph = st.columns(2)
 
 # First, the data
 col_data.markdown(f"### {ConfigLang['Title_data']}")
-ConfigIo['df'] = col_data.data_editor(df_e,
+ConfigIo['df_edges'] = col_data.data_editor(DfEdges,
                      num_rows="dynamic",
                      hide_index = True,
-                     key = 'df_e_editor',
+                     key = 'DfEdges_editor',
                      on_change=None
                      )
 
-dl_csv_data = df_to_csv(df_e)
+dl_csv_data = df_to_csv(DfEdges)
 Engines = list(ConfigDigraph['RenderOptions'].keys())
 col_data.download_button(
-    label=ConfigLang['Label_download'],
+    label=ConfigLang['LabelGetEdges'],
     data=dl_csv_data,
     file_name="my-systemic-diagram.csv",
     mime="text/csv"
     )
-#col_data.button(label=ConfigLang['Label_update'], on_click=UpdateEdges)
+
+"""Get list of Nodes from edges descriptions"""
+nodes = ConfigIo['df_edges']['Node1'].combine_first(ConfigIo['df_edges']['Node2']).to_list()
+nodes = "\n".join(nodes)
+col_data.download_button(
+    label=ConfigLang['LabelGetNodes'],
+    data=nodes.encode('utf-8'),
+    file_name='my-systemic-nodes.csv',
+    mime='text/csv'
+    )
 
 # Second, the graph
 col_graph.markdown(f"### {ConfigLang['Title_graph']}")

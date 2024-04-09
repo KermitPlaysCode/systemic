f5cefc7799d906217e1465b99d17044233cb129d
Olivier <78881889+KermitPlaysCode@users.noreply.github.com>
2024-04-09 23:15:01 +0200

    Qques simplifications; suppr titres trop gros

--- stream-systemic.py
+++ stream-systemic.py
@@ -2,11 +2,6 @@ import streamlit as st
 import graphviz
 import pandas as pd
 
-NodesTypeToShape = {
-    '':'box',
-    'Stock': 'box',
-    'Influencer': 'point'
-}
 NodesTypeToColor = {
     'Stock': 'black',
     'Influencer': 'white'
@@ -88,12 +83,10 @@ def UpdateGraph_v2() -> None:
     """Update the graph based on data"""
     # lists : configured clusters and created subgraphs
     list_clusters = ConfigIo['df_nodes']['Cluster'].dropna().unique().tolist()
-    if "main" in list_clusters:
-        list_clusters.remove("main")
-    # Clear diagram main
-    ConfigDigraph['graphes']['main'].clear()
-    # Create 1 subgraph per cluster
+    if 'main' not in list_clusters:
+        list_clusters.append('main')
     CDkeys = ConfigDigraph['graphes'].keys()
+    # Manage graphs: create if new, else clear
     for cl in list_clusters:
         if cl not in CDkeys:
             ConfigDigraph['graphes'][cl] = graphviz.Digraph(
@@ -107,15 +100,15 @@ def UpdateGraph_v2() -> None:
         else:
             ConfigDigraph['graphes'][cl].clear()
 
-    # Prepare data using edges and nodes infos
+    # Retrieve cluster name of edges
     df_rework = ConfigIo['df_edges'].join(ConfigIo['df_nodes'].set_index('Node').drop('NodeType', axis=1), on='Node1').rename(columns={'Cluster': 'ClusterNode1'})
     df_rework = df_rework.join(ConfigIo['df_nodes'].set_index('Node').drop('NodeType', axis=1), on='Node2').rename(columns={'Cluster': 'ClusterNode2'})
     df_rework.fillna(value="main", inplace=True)
-    # Prepare nodes
+    # create and configure nodes
     for tup in DfNodes.itertuples(index=False):
         (node, cluster, nodetype) = tup
         ConfigDigraph['graphes'][cluster].node(name=node, color=NodesTypeToColor[nodetype], label=node)
-    # Go through data to process edges
+    # Create and configure edges
     count = 0
     for row in df_rework.itertuples(index=False):
         count += 1
@@ -124,24 +117,23 @@ def UpdateGraph_v2() -> None:
         effect = row.Effet
         c1 = row.ClusterNode1
         c2 = row.ClusterNode2
+        tgt_c = ''
         # 2 noeuds dans un même cluster de subgraph:
         test_values = n1 not in ["", None] and n2 not in ["", None] and effect in ['+','-']
         if test_values and c1 == c2:
-            ConfigDigraph['graphes'][c1].edge(
-                n1,n2,
-                label=effect,
-                color=ConfigDigraph['Effet'][effect]
-                )
+            tgt_c = c1
         # 2 nodes dans différents clsuters OU dans graph
         elif test_values:
-            ConfigDigraph['graphes']['main'].edge(
-                n1,n2,
-                label=effect,
-                color=ConfigDigraph['Effet'][effect]
-                )
+            tgt_c = 'main'
+        ConfigDigraph['graphes'][tgt_c].edge(
+            n1,n2,
+            label=effect,
+            color=ConfigDigraph['Effet'][effect]
+            )
+    list_clusters.remove('main')
     for cl in list_clusters:
         ConfigDigraph['graphes']['main'].subgraph(ConfigDigraph['graphes'][cl])
-        ConfigDigraph['graphes'][cl].render()
+        #ConfigDigraph['graphes'][cl].render()
     if ConfigDigraph['flatten']:
         fn = ConfigDigraph['graphes']['main'].unflatten(stagger = 2).render()
     else:
@@ -193,12 +185,13 @@ tab_txt, tab_grf = st.tabs([ConfigLang['TabText'],ConfigLang['TabGraph']])
 col_graph = tab_grf # trick as I added tabs
 
 # DATA : first, populate the data column
-## Title and editable pandas for edges
-tab_txt.markdown(f"## {ConfigLang['TabText']}")
+## Title
+# tab_txt.markdown(f"## {ConfigLang['TabText']}") # removed because it's useless and big
 col_data_edges, col_data_nodes, col_config = tab_txt.columns(3)
 col_data_edges.markdown(f"### {ConfigLang['Title_data_egdes']}")
 col_data_nodes.markdown(f"### {ConfigLang['Title_data_nodes']}")
 
+## Editable pandas for edges
 ConfigIo['df_edges'] = col_data_edges.data_editor(DfEdges,
                     num_rows="dynamic",
                     hide_index = True,
@@ -214,7 +207,7 @@ ConfigIo['df_nodes'] = col_data_nodes.data_editor(DfNodes,
 
 # GRAPH: second, the graph column
 ## Title and graph place
-tab_grf.markdown(f"## {ConfigLang['Title_graph']}")
+# tab_grf.markdown(f"## {ConfigLang['Title_graph']}") # removed because it's useless and big
 ConfigIo['Container'] = col_graph.divider()
 
 # CONFIG: third, the config column
